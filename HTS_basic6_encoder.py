91e474adbfe100d07b29f48881d50d8ba7991735
KChuene <pk1110@yahoo.com>
2024-08-27 21:34:18 +0200

    Added decoding functionality; decode(), dec_numeric() and dec_alpha() functions.

--- HTS_basic6_encoder.py
+++ HTS_basic6_encoder.py
@@ -1,12 +1,21 @@
 import sys
 
+def usage():
+    print("""
+    usage: 
+        program.py -dec M3ss@ge2Enc0de
+        program.py -enc M3ss@ge2Enc0de
+    """)
+    print("\n** Note ** Messages are capped at length of 14 chars.")
+
 def bye(msg: str, show_usg: bool):
     if msg:
         print(msg)
 
     if show_usg:
-        print("usage: \n\tprogram.py M3ss@ge2Enc0de")
-        print("\n** Note ** Messages are capped at length of 14 chars.")
+        usage()
+
+    sys.exit()
 
 def enc_numeric(char: str, index: int):
     if not char.isnumeric():
@@ -18,13 +27,33 @@ def enc_numeric(char: str, index: int):
     else:
         return str(numeric)
     
+def dec_numeric(char: str, index: int):
+    if not char.isnumeric():
+        return char
+    
+    numeric = int(char) - index
+    if numeric < 0:
+        return char
+    else:
+        return str(numeric)
+    
 def enc_alpha(char: str, index: int):
     if char.isnumeric():
         return char
     
     return chr( ord(char) + index )
 
-def encode(string: str):
+def dec_alpha(char: str, index: int):
+    if char.isnumeric():
+        return char
+    
+    convert = ord(char) - index
+    if convert < 0:
+        return char
+    else:
+        return chr( convert )
+
+def encode(string: str) -> str:
     result = []
     for char in string:
         if char.isnumeric():
@@ -34,8 +63,27 @@ def encode(string: str):
 
     return ''.join(result)
 
-if __name__=="__main__":
-    if not (len(sys.argv) >= 2):
-        bye("[!] Expected string to encrypt but none found.", True) 
+def decode(string: str) -> str:
+    result = []
+    for char in string:
+        if char.isnumeric():
+            result.append( dec_numeric(char, string.index(char)) )
+        else:
+            result.append( dec_alpha(char, string.index(char)) )
 
-    print(encode(sys.argv[1]))
+    return ''.join(result)    
+
+if __name__=="__main__":
+    if not (len(sys.argv) >= 3):
+        bye("Insufficient arguments.", True)
+
+    elif len(sys.argv[2]) > 14:
+        bye("Input string is too long.")
+
+    mode = sys.argv[1]
+    if mode == "-enc":
+        print(encode(sys.argv[2]))
+    elif mode == "-dec":
+        print(decode(sys.argv[2]))
+    else:
+        bye("Only modes -enc and -dec are applicable.")
